a2f08b24aaba68f9f66b954386a89e05d599fd02
tatiana <sorokina.tatiana.ev@gmail.com>
2019-04-13 21:33:57 +0200

    update func name

--- src/main/java/rest/BlogServiceRepository.java
+++ src/main/java/rest/BlogServiceRepository.java
@@ -2,15 +2,15 @@ package rest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import rest.Blog;
 
 import java.util.List;
+
 
 @Repository
 public interface BlogServiceRepository extends JpaRepository<Blog, Integer> {
 
     // custom query to search to blog post by title or content
-    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+    List<Blog> findByTitleOrContent(String text, String textAgain);
     public Blog save(Blog blog);
 
 }
